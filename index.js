2769c93dc4d89cc785a985b881a5359a3db53133
paul alexander diaz velez <108167418+poldiaz50@users.noreply.github.com>
2026-01-02 00:01:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minute");
 const secondEl = document.getElementById("second");
 
-const newYearTime = new Date("Jan 1, 2026 00:00:00").getTime();
+const newYearTime = new Date("Jan 1, 2027 00:00:00").getTime();
 
 updateCountdown();
 
